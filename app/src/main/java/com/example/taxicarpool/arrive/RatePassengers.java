1c6cbe96110f01cc7045aacee217da45e25ab774
fsolier <bulacc1@mcmaster.ca>
2023-04-05 18:14:57 -0400

    PayNow Arrival

---
+++ app/src/main/java/com/example/taxicarpool/arrive/RatePassengers.java
@@ -0,0 +1,14 @@
+package com.example.taxicarpool.arrive;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.taxicarpool.R;
+import android.os.Bundle;
+
+public class RatePassengers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate_passengers);
+    }
+}
