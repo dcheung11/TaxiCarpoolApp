2860c313d9d02ca7ea13b443f1fef98218dbc475
HamidHarris <harrishamid1990@gmail.com>
2023-04-09 12:32:02 -0400

    added encryption methods and login

--- app/src/main/java/com/example/taxicarpool/data/EncryptionController.java
+++ app/src/main/java/com/example/taxicarpool/data/EncryptionController.java
@@ -2,7 +2,11 @@ package com.example.taxicarpool.data;
 
 import android.content.Context;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
+import javax.crypto.SecretKey;
+import javax.crypto.KeyGenerator;
+import javax.crypto.Cipher;
 
 public class EncryptionController {
 
@@ -10,9 +14,21 @@ public class EncryptionController {
     private static AppDatabase db;
     private static UserDao dao;
 
+    private static final String CRYPTO_ALGORITHM = "AES";
+    private static final int CRYPTO_KEY_SIZE = 128;
+    private SecretKey secretKey;
+    private Cipher cipher;
 
+    public EncryptionController() throws Exception {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance(CRYPTO_ALGORITHM);
+        keyGenerator.init(CRYPTO_KEY_SIZE);
+        secretKey = keyGenerator.generateKey();
 
-    public static synchronized EncryptionController getInstance(Context context) {
+        // Create a cipher object
+        cipher = Cipher.getInstance(CRYPTO_ALGORITHM);
+    }
+
+    public static synchronized EncryptionController getInstance(Context context) throws Exception {
 
         if (instance == null) {
             instance = new EncryptionController();
@@ -55,8 +71,25 @@ public class EncryptionController {
         return dao.getCarpoolWithRiders(matchId);
     }
 
-    public void encrypt(){
+    public byte[] encrypt(String input) throws Exception{
+        //encryption mode
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
 
+        // Encrypt the input string
+        byte[] inputBytes = input.getBytes();
+        byte[] encryptedBytes = cipher.doFinal(inputBytes);
+
+        return encryptedBytes;
+    }
+
+    public String decrypt(byte[] input) throws Exception{
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+
+        // Decrypt the input bytes
+        byte[] decryptedBytes = cipher.doFinal(input);
+        String decryptedString = new String(decryptedBytes);
+
+        return decryptedString;
     }
 
 }
