d667aade9a4538a533e52aeb8a2c16088f532025
DangJustin <justin.dang6024@gmail.com>
2023-04-10 16:16:03 -0400

    Login Screen Complete

--- app/src/main/java/com/example/taxicarpool/MainActivity.java
+++ app/src/main/java/com/example/taxicarpool/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.taxicarpool.arrive.ArrivalActivity;
 import com.example.taxicarpool.create.CreateCarpoolActivity;
@@ -25,8 +26,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openArrive(View v){
-        Intent i = new Intent(this,ArrivalActivity.class);
-        startActivity(i);
+        if (!LoggedInUser.getInstance().isLoggedIn()){
+            Toast.makeText(this, "Must be Logged In", Toast.LENGTH_SHORT).show();
+        } else {
+            Intent i = new Intent(this,ArrivalActivity.class);
+            startActivity(i);
+        }
     }
 
     public void openCreate(View v){
@@ -38,12 +43,6 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(this, JoinCarpoolActivity.class);
         startActivity(i);
     }
-
-
-
-
-
-
 
 
 }
--- app/src/main/java/com/example/taxicarpool/join/MatchMaker.java
+++ app/src/main/java/com/example/taxicarpool/join/MatchMaker.java
@@ -1,11 +1,22 @@
 package com.example.taxicarpool.join;
 
+import android.content.Context;
+
 import com.example.taxicarpool.data.Carpool;
+import com.example.taxicarpool.data.EncryptionController;
 import com.example.taxicarpool.data.UserIdentity;
 
-public class MatchMaker {
+import java.util.List;
 
-    public Carpool findCarpool(UserIdentity user){
+public class MatchMaker {
+    Context context;
+
+    public MatchMaker(Context context){
+        this.context = context;
+    }
+
+    public Carpool findCarpool(UserIdentity user) throws Exception {
+        List<Carpool> allCarpools = EncryptionController.getInstance(context).getAllCarpool();
         return null;
     }
 
